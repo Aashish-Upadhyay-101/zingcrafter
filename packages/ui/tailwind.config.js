22db01867ba169d36a112c9b537104d5a51c1b48
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-12-25 22:03:37 +0545

    chore: minor tweaks in packages

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -2,9 +2,5 @@ const baseConfig = require("@tracksubz/tailwind-config");
 
 module.exports = {
   ...baseConfig,
-  content: [
-    ...baseConfig.content,
-    "./components/**/*.{ts,tsx}",
-    "./lib/**/*.{ts,tsx}",
-  ],
+  content: [...baseConfig.content, "./components/**/*.{ts,tsx}", "./lib/**/*.{ts,tsx}"],
 };
